479d52c16555583c224b46d029aa8c2f3e43f8ed
kevinzhu2019 <kevinzhu.zyk@gmail.com>
2020-04-22 01:06:27 +0000

    creating battleship chess plate

---
+++ battleship.js
@@ -0,0 +1,10 @@
+const battleShip = () => {
+  const battleField = [];
+  battleField.push('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j');
+  console.log(battleField);
+  for (let i = 0; i < battleField.length; i++) {
+    
+  }
+};
+
+battleShip();
